91680602d7cb8da42033165bcb4636d569b26679
Pratyay Ganguli <pganguli5520@gmail.com>
2026-09-12 13:27:58 +0530

    ci(): add blank lines for improved readability in main.py files

--- python/data/main.py
+++ python/data/main.py
@@ -1,8 +1,9 @@
 # Getting started with writing structural code with Python
 
+
 def main():
     print("Gripping on Python")
 
+
 if __name__ == "__main__":
     main()
-
--- python/opensearch-log-agent/main.py
+++ python/opensearch-log-agent/main.py
@@ -5,9 +5,11 @@ import logging
 def init():
     logging.basicConfig(level=logging.DEBUG)
 
+
 def main():
     init()
     logging.debug("starting opensearch log agent")
+
 
 if __name__ == "__main__":
     main()
