3d9d2488eea345f2fd0e675be171d5d1b7cbeb89
Pratyay Ganguli <pganguli5520@gmail.com>
2026-09-14 20:12:17 +0530

    fix((): remove unnecessary logger synchronization function

--- golang/main.go
+++ golang/main.go
@@ -12,12 +12,6 @@ func main() {
 	if logger, err := zap.NewProduction(); err != nil {
 		log.Fatalf("could not use production zap logger")
 	} else {
-		loggerSync := func() {
-			if err := logger.Sync(); err != nil {
-				panic(err)
-			}
-		}
-		defer loggerSync()
 		s := fmt.Sprintf("successfully containerized the backend code - %s", time.Now().String())
 		logger.Info(s)
 	}
