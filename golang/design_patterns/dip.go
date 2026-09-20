b6b868f07b6151a0dfec8b7f5c4d743bddf7585a
Pratyay Ganguli <pganguli5520@gmail.com>
2026-09-20 21:21:18 +0530

    feat(): update VSCode theme to Ayu Dark, add CDN documentation, and implement Dependency Inversion example

---
+++ golang/design_patterns/dip.go
@@ -0,0 +1,13 @@
+package designpatterns
+
+import "fmt"
+
+// dependency inversion principle -
+// dependency principle states that high level modules and low level modules should
+// not be tightly coupled with each other, in simple words high level classes should not
+// depend on the low level classes directly
+
+func DependencyInversionExample() {
+	// todo: add the dependency inversion principle logic
+	fmt.Println("Will be adding the example for dependency inversion example soon!")
+}
